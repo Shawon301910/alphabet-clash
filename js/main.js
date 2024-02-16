559bedc0a4fd2c37c656af7641057858c7367f6f
Shawon301910 <shawonkumarsarkar.bd@gmail.com>
2024-02-16 21:44:33 +0600

    complete button press add classList

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function starting() {
+    const home = document.getElementById('home');
+    home.classList.add('hidden')
+
+    const playground = document.getElementById('playground');
+    playground.classList.remove('hidden')
+    playground.classList.add('flex')
+}
