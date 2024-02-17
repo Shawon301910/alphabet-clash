8e3ff924f6c925b6a42a662c67a8b03835951577
Shawon301910 <shawonkumarsarkar.bd@gmail.com>
2024-02-17 15:11:50 +0600

    almost finished

--- js/first.js
+++ js/first.js
@@ -25,3 +25,28 @@ function setBgColor(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-red-300');
 }
+
+function removeBgColor(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-red-300');
+}
+
+function getTextElement(elementId) {
+    const currentText = document.getElementById(elementId);
+    const text = currentText.innerText;
+    const value = parseInt(text);
+    return value;
+}
+
+function showTextElement(elementId, value) {
+    const currentText = document.getElementById(elementId);
+    currentText.innerText = value;
+}
+
+function finalScore() {
+    const getText = getTextElement('game-finished');
+    const getScore = document.getElementById('score-update').innerText;
+    const getScoreInt = parseInt(getScore);
+    const score = getText + getScoreInt;
+    showTextElement('game-finished', score)
+}
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ function gaming() {
     hideElementById('home');
     showElementById('playground');
     playing();
+    document.addEventListener('keyup', playerPressed);
 }
 
 function playing() {
@@ -12,4 +13,43 @@ function playing() {
 
     const bgColor = setBgColor(alphabet);
     return bgColor;
+}
+
+function playerPressed(event) {
+    const pressed = event.key;
+
+    if (pressed === 'Escape') {
+        hideElementById('playground');
+        showElementById('score-section');
+    }
+
+    const get = document.getElementById('change-alphabet').innerText;
+
+    if (get === pressed) {
+        const updateScore = getTextElement('score-update');
+        const score = updateScore + 1;
+        showTextElement('score-update', score)
+
+        removeBgColor(pressed);
+        playing();
+        return score;
+    } else {
+        const updateLife = getTextElement('lost-life');
+        const life = updateLife - 1;
+        showTextElement('lost-life', life)
+
+        if (life === 0) {
+            finalScore();
+            hideElementById('playground');
+            showElementById('score-section');
+        }
+    }
+}
+
+function playAgain() {
+    showTextElement('lost-life', 3)
+    showTextElement('score-update', 0)
+    showTextElement('game-finished', 0)
+    hideElementById('score-section');
+    showElementById('playground');
 }
